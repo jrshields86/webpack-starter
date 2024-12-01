a5b29814fe9da946ecaf52177b4df41af7a515aa
jrshields86 <jrshields86@gmail.com>
2024-12-01 13:17:59 -0700

    three quater of the way

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,19 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: '[name] [contenthash].js',
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
     },
     module: {
         rules: [
@@ -21,6 +33,16 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
         ],
     },
